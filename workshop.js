1d974caefa75f020d56573f11ec0772e38ebc463
Halotitee <moreseed01@gmail.com>
2017-04-05 22:23:52 +0000

    solved some

--- workshop.js
+++ workshop.js
@@ -65,7 +65,15 @@ function every(predicate, theArray) {
 }
 
 function some(predicate, theArray) {
-
+    if(theArray.length === 0) {
+      return false;
+    }
+    for (var i = 0; i < theArray.length; i++){
+      if (predicate(theArray[i])) {
+        return true;
+      }
+    }
+    return false;
 }
 
 function indexOf(item, theArray) {
